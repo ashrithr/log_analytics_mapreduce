c93819f551c79366e3dc983f9ea80e368a14b544
ashrithr <ashrith@me.com>
2014-08-13 12:12:23 -0700

    Add resolveip code for parsing ip to loc

---
+++ src/main/java/com/cloudwick/mapreduce/loganalytics/resolveip/Driver.java
@@ -0,0 +1,64 @@
+package com.cloudwick.mapreduce.loganalytics.resolveip;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.net.URI;
+
+/**
+ * MapReduce driver for resolveing ip to location
+ *
+ * Running:
+ * 1. Add src/main/resources/GeoLite2-City.mmdb to hdfs
+ *
+ * @author ashrith
+ */
+public class Driver extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output> \n", getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Configuration conf = getConf();
+        conf.set("geoip.filename", "GeoLite2-City.mmdb");
+
+        Job job = Job.getInstance(conf);
+        job.setJobName("Log Parser");
+        job.setJarByClass(getClass());
+        job.addCacheFile(new URI("hdfs://localhost:8020/" + conf.get("geoip.filename") + "#" + conf.get("geoip.filename")));
+
+        URI[] cacheFiles = job.getCacheFiles();
+        if (cacheFiles != null) {
+            for (URI cachefile: cacheFiles) {
+                System.out.println("Cache file ->" + cachefile);
+            }
+        }
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(ResolveIPMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] argv) throws Exception {
+        System.exit(ToolRunner.run(new Driver(), argv));
+    }
+}
